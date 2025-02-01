97bdc873d9d4c78decca0046188364d06933aa58
Luis Eduardo <luisedu.lepa@gmail.com>
2025-01-31 23:40:11 -0300

    mudança na config do next.config.mjs para funcionamento da Image do Nextjs corretamente

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost']
+        domains: [process.env.NEXT_PUBLIC_BASEURL]
     },
     experimental: {
         missingSuspenseWithCSRBailout: false,
