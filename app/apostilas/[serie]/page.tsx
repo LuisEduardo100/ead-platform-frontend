3d04d1120e0d737c502426e51666b2e0243bf490
Luis Eduardo <luisedu.lepa@gmail.com>
2025-02-18 23:36:46 -0300

    mudança na seção de pastas - seocndary name e code adicionados para melhor identificação

--- app/apostilas/[serie]/page.tsx
+++ app/apostilas/[serie]/page.tsx
@@ -1,97 +1,107 @@
-'use client'
-import { useParams, useRouter } from 'next/navigation'; // Captura parâmetros da rota
-import { useEffect, useState } from 'react';
-import { CategoryType } from '../../../src/services/categoriesService';
-import episodeFileService from '../../../src/services/episodeFileService';
-import { CourseType } from '../../../src/services/courseService';
-import HandoutNavigation from '../../../src/components/common/navigationHandouts';
-import { Button, Container } from 'reactstrap';
-import HeaderAuth from '../../../src/components/HomeAuth/header';
-import styles from '../../styles/serieStyle.module.scss';
-import { Folder } from '@mui/icons-material';
-import { useMenu } from '../../../src/components/common/menu/menuProvider';
-import FooterAuth from '../../../src/components/HomeAuth/footerAuth';
-import PageSpinner from '../../../src/components/common/pageSpinner';
+"use client";
+import { useParams, useRouter } from "next/navigation"; // Captura parâmetros da rota
+import { useEffect, useState } from "react";
+import { CategoryType } from "../../../src/services/categoriesService";
+import episodeFileService from "../../../src/services/episodeFileService";
+import { CourseType } from "../../../src/services/courseService";
+import HandoutNavigation from "../../../src/components/common/navigationHandouts";
+import { Button, Container } from "reactstrap";
+import HeaderAuth from "../../../src/components/HomeAuth/header";
+import styles from "../../styles/serieStyle.module.scss";
+import { Folder } from "@mui/icons-material";
+import { useMenu } from "../../../src/components/common/menu/menuProvider";
+import FooterAuth from "../../../src/components/HomeAuth/footerAuth";
+import PageSpinner from "../../../src/components/common/pageSpinner";
 
 export default function SeriePage() {
-    const { serie } = useParams(); // Captura o parâmetro da rota dinâmica
-    const [courses, setCourses] = useState<CourseType[]>([]);
-    const [data, setData] = useState<CategoryType[]>([]); // Dados completos
-    const router = useRouter()
-    const [selected, setSelected] = useState<number | string>(); // Mover o estado para fora do map
-    const { isMenuOpen } = useMenu()
-    const [loading, setLoading] = useState(true)
+  const { serie } = useParams(); // Captura o parâmetro da rota dinâmica
+  const [courses, setCourses] = useState<CourseType[]>([]);
+  const [data, setData] = useState<CategoryType[]>([]); // Dados completos
+  const router = useRouter();
+  const [selected, setSelected] = useState<number | string>(); // Mover o estado para fora do map
+  const { isMenuOpen } = useMenu();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setLoading(false);
-        }, 500)
-        return () => clearInterval(intervalID)
-    }, [])
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setLoading(false);
+    }, 500);
+    return () => clearInterval(intervalID);
+  }, []);
 
-    useEffect(() => {
-        const getAllApostila = async () => {
-            try {
-                const response = await episodeFileService.getAllFiles(); // Chama a API
+  useEffect(() => {
+    const getAllApostila = async () => {
+      try {
+        const response = await episodeFileService.getAllFiles(); // Chama a API
 
-                if (!response) {
-                    console.error("Data não recebida!");
-                    return;
-                }
+        if (!response) {
+          console.error("Data não recebida!");
+          return;
+        }
 
-                setData(response.data); // Atualiza os dados recebidos
-            } catch (error) {
-                console.error("Erro ao buscar apostilas:", error);
-            }
-        };
-
-        getAllApostila();
-    }, []); // Executa apenas ao montar o componente
-
-    useEffect(() => {
-        if (!serie || data.length === 0) return;
-
-        // Decodifica o valor da série (caso esteja codificado como %C2%BA, %20, etc.)
-        const decodedSerie = decodeURIComponent(String(serie));
-
-        // Filtrar cursos pela série atual
-        const filteredCourses = data
-            .flatMap((category: CategoryType) => category.courses || []) // Obtem todos os cursos
-            .filter(course => course?.serie === decodedSerie); // Filtra pela série da URL
-
-
-        setCourses(filteredCourses); // Atualiza os cursos filtrados
-    }, [serie, data]); // Executa quando "serie" ou "data" mudar
-
-    function handleNavigation(courseId: string | number) {
-        router.push(`/apostilas/${decodeURIComponent(String(serie))}/${courseId}`)
-    }
-
-    const handleClick = (courseId: number | string) => {
-        setSelected(courseId);
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar apostilas:", error);
+      }
     };
 
-    if (loading || !data) return <PageSpinner />
-    return (
-        <main className={`${styles.main} ${isMenuOpen ? styles.menuOpen : ""}`}>
-            <HeaderAuth />
-            <div className={styles.mainContent}>
-                <HandoutNavigation serie={decodeURIComponent(String(serie))} topic={null} />
-                <ul className={styles.ulDiv}>
-                    {courses.map(course => (
-                        <li
-                            key={course?.id}
-                            className={`${styles.liDiv} ${selected === course?.id ? styles.selected : ''}`}
-                            onClick={() => handleClick(course?.id)}
-                            onDoubleClick={() => handleNavigation(course?.id)}
-                        >
-                            <Folder className={styles.folderLi}/>
-                            <Button className={styles.buttonStyle}>{course?.name}</Button>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <FooterAuth/>
-        </main>
-    );
+    getAllApostila();
+  }, []); // Executa apenas ao montar o componente
+
+  useEffect(() => {
+    if (!serie || data.length === 0) return;
+
+    const decodedSerie = decodeURIComponent(String(serie));
+
+    const filteredCourses = data
+      .flatMap((category: CategoryType) => category.courses || [])
+      .filter((course) => course?.serie === decodedSerie);
+
+    setCourses(filteredCourses);
+  }, [serie, data]);
+
+  function handleNavigation(courseId: string | number) {
+    router.push(`/apostilas/${decodeURIComponent(String(serie))}/${courseId}`);
+  }
+
+  const handleClick = (courseId: number | string) => {
+    setSelected(courseId);
+  };
+
+  if (loading || !data) return <PageSpinner />;
+  return (
+    <main className={`${styles.main} ${isMenuOpen ? styles.menuOpen : ""}`}>
+      <HeaderAuth />
+      <div className={styles.mainContent}>
+        <HandoutNavigation
+          serie={decodeURIComponent(String(serie))}
+          topic={null}
+        />
+        <ul className={styles.ulDiv}>
+          {courses.map((course) => (
+            <li
+              key={course?.id}
+              className={`${styles.liDiv} ${
+                selected === course?.id ? styles.selected : ""
+              }`}
+              onClick={() => handleClick(course?.id)}
+              onDoubleClick={() => handleNavigation(course?.id)}
+            >
+              <div className={styles.topRow}>
+                <Folder className={styles.folderLi} />
+                <div className={styles.divTextos}>
+                  <Button className={styles.buttonStyle}>{course?.name}</Button>
+                  <div className={styles.bottomRow}>
+                    <p>{course?.secondaryName}</p>
+                    <p>{course?.code}</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <FooterAuth />
+    </main>
+  );
 }
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,192 +1,214 @@
-import api from './api'
-import { CategoryType } from './categoriesService';
+import api from "./api";
+import { CategoryType } from "./categoriesService";
 
 export type EpisodeType = {
-    id: number;
-    name: string;
-    synopsis: string;
-    order: number;
-    videoUrl: string;
-    quizz: QuizzType[]
-    secondsLong: number;
-    Files: EpisodeFileType[];
-}
+  id: number;
+  name: string;
+  synopsis: string;
+  order: number;
+  videoUrl: string;
+  quizz: QuizzType[];
+  secondsLong: number;
+  Files: EpisodeFileType[];
+};
 export type EpisodeTypeAdapted = {
-    id: number;
-    name: string;
-    synopsis: string;
-    order: number;
-    videoUrl: string;
-    quizz: QuizzType[]
-    secondsLong: number;
-    files: EpisodeFileType[];
-}
+  id: number;
+  name: string;
+  synopsis: string;
+  order: number;
+  videoUrl: string;
+  quizz: QuizzType[];
+  secondsLong: number;
+  files: EpisodeFileType[];
+};
 export type CourseType = {
-    id: number;
-    name: string;
-    featuredName: string;
-    synopsis: string;
-    serie: string;
-    thumbnailUrl: string;
-    featuredImage: string;
-    Episodes?: EpisodeType[];
-    watchStatus?: WatchStatus[]
-    liked: boolean
-    favorited: boolean
-}
+  id: number;
+  name: string;
+  secondaryName: string;
+  code: string;
+  featuredName: string;
+  synopsis: string;
+  serie: string;
+  thumbnailUrl: string;
+  featuredImage: string;
+  Episodes?: EpisodeType[];
+  watchStatus?: WatchStatus[];
+  liked: boolean;
+  favorited: boolean;
+};
 export type CourseQuizzType = {
-    id: number
-    name: string
-    Quizzes: QuizzType[]
-}
+  id: number;
+  name: string;
+  Quizzes: QuizzType[];
+};
 export type QuizzType = {
-    order: number
-    question: string
-    answers: string[]
-    fileUrl?: string
-    correctAnswer: number
-    serie: string
-    dificuldade: string
-}
+  order: number;
+  question: string;
+  answers: string[];
+  fileUrl?: string;
+  correctAnswer: number;
+  serie: string;
+  dificuldade: string;
+};
 export type WatchStatus = {
-    isWatching: boolean
-    episodeId: number
-}
+  isWatching: boolean;
+  episodeId: number;
+};
 export type CourseTypeWithNoEps = {
-    id: number;
-    name: string;
-    serie: string;
-    featuredName: string;
-    thumbnailUrl: string;
-    synopsis: string;
-}
+  id: number;
+  name: string;
+  serie: string;
+  featuredName: string;
+  thumbnailUrl: string;
+  synopsis: string;
+};
 export type EpisodeFileType = {
-    id: number;
-    episodeId: EpisodeFileType;
-    name: string;
-    url: string[]
-    fileUrl: string[] // conferir se da erro em outras paginas
-    course: string
-    serie: string
-    category: string
-}
+  id: number;
+  episodeId: EpisodeFileType;
+  name: string;
+  url: string[];
+  fileUrl: string[]; // conferir se da erro em outras paginas
+  course: string;
+  serie: string;
+  category: string;
+};
 export type EpisodeFileTypeAdapted = {
-    id: number;
-    episodeId: EpisodeFileType;
-    name: string;
-    fileUrl: string[]
-    course: string
-    serie: string
-}
-
+  id: number;
+  episodeId: EpisodeFileType;
+  name: string;
+  fileUrl: string[];
+  course: string;
+  serie: string;
+};
 
 const courseService = {
-    getNewestCourses: async () => {
-        const res = await api.get("/courses/newest").catch((error) => {
-            return error.response
-        })
-        return res
-    },
-    getFeaturedCourses: async () => {
-        const token = sessionStorage.getItem("vocenotadez-token")
-        const res = await api.get('/courses/featured', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            })
-            .catch((error) => {
-                return error.message
-            })
-        return res
-    },
-    addToFav: async (courseId: number | string) => {
-        const token = sessionStorage.getItem("vocenotadez-token")
-        const res = await api.post('/favorites' , {courseId}, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            },
-            data: {courseId}
-        }).catch((error) => {
-            return error.response
-        })
-        return res
-    },
-    removeFav: async (courseId: number | string) => {
-        try {
-          const token = sessionStorage.getItem("vocenotadez-token");
-    
-          const favorite = await api.delete(`/favorites/${courseId}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          return favorite;
-        } catch (error: any) {
-          return error.response;
+  getNewestCourses: async () => {
+    const res = await api.get("/courses/newest").catch((error) => {
+      return error.response;
+    });
+    return res;
+  },
+  getFeaturedCourses: async () => {
+    const token = sessionStorage.getItem("vocenotadez-token");
+    const res = await api
+      .get("/courses/featured", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.message;
+      });
+    return res;
+  },
+  addToFav: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("vocenotadez-token");
+    const res = await api
+      .post(
+        "/favorites",
+        { courseId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          data: { courseId },
         }
-      },
-    like: async (courseId: number | string) => {
-        try {
-            const token = sessionStorage.getItem("vocenotadez-token");
-            const like = await api.post(
-                "/likes",
-                { courseId },
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    }
-                }
-            );
-            return like;
-        } catch (error: any) {
-            return error.response;
-        }
-    },
-    removeLike: async (courseId: number | string) => {
-        try {
-            const token = sessionStorage.getItem("vocenotadez-token");
+      )
+      .catch((error) => {
+        return error.response;
+      });
+    return res;
+  },
+  removeFav: async (courseId: number | string) => {
+    try {
+      const token = sessionStorage.getItem("vocenotadez-token");
 
-            const like = await api.delete(`/likes/${courseId}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                data: {courseId}
-            });
-            return like;
-        } catch (error: any) {
-            return error.response;
-        }
-    },
-    getFavCourses: async () => {
-        const token = sessionStorage.getItem("vocenotadez-token")
-        const res = api.get('/favorites', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).catch((error) => { return error.message })
-        return res
-    },
-    getSearch: async (name: string, year: string | null) => {
-        const query = name ? `/courses/search?name=${name}&serie=${year}` : `/courses/search?name=&serie=${year}`;
-        const res = await api.get(query).catch((error) => { return "Erro aqui"+error.message });
-        return res;
-        
-    },
-    getSearchGeneral: async (name: string) => {
-        const res = await api.get(`/courses/gsearch?name=${name}`).catch((error) => { return "Erro aqui"+error.message });
-        return res;
-        
-    },
-    getEpisodes: async (id: number | string) => {
-        const token = sessionStorage.getItem("vocenotadez-token")
-
-        const res = await api.get(`/courses/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).catch((error) => { return error.message })
-        return res
+      const favorite = await api.delete(`/favorites/${courseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return favorite;
+    } catch (error: any) {
+      return error.response;
     }
-}
+  },
+  like: async (courseId: number | string) => {
+    try {
+      const token = sessionStorage.getItem("vocenotadez-token");
+      const like = await api.post(
+        "/likes",
+        { courseId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return like;
+    } catch (error: any) {
+      return error.response;
+    }
+  },
+  removeLike: async (courseId: number | string) => {
+    try {
+      const token = sessionStorage.getItem("vocenotadez-token");
 
-export default courseService
+      const like = await api.delete(`/likes/${courseId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: { courseId },
+      });
+      return like;
+    } catch (error: any) {
+      return error.response;
+    }
+  },
+  getFavCourses: async () => {
+    const token = sessionStorage.getItem("vocenotadez-token");
+    const res = api
+      .get("/favorites", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.message;
+      });
+    return res;
+  },
+  getSearch: async (name: string, year: string | null) => {
+    const query = name
+      ? `/courses/search?name=${name}&serie=${year}`
+      : `/courses/search?name=&serie=${year}`;
+    const res = await api.get(query).catch((error) => {
+      return "Erro aqui" + error.message;
+    });
+    return res;
+  },
+  getSearchGeneral: async (name: string) => {
+    const res = await api
+      .get(`/courses/gsearch?name=${name}`)
+      .catch((error) => {
+        return "Erro aqui" + error.message;
+      });
+    return res;
+  },
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("vocenotadez-token");
+
+    const res = await api
+      .get(`/courses/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        return error.message;
+      });
+    return res;
+  },
+};
+
+export default courseService;
